e36bc6134f4f4c190661deb3d0446c1cf1b1d591
AkashSDas <aakashdas368@gmail.com>
2021-04-22 20:29:02 +0530

    add style classes to Navbar.tsx

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,5 +1,8 @@
-import { MouseEventHandler, useState } from "react";
+import { MouseEventHandler, useEffect, useState } from "react";
+import { getThemeFromLocalStorage } from "../../lib/theme";
+import MoonSVG from "../svg/MoonSVG";
 import ScrollSVG from "../svg/ScrollSVG";
+import SunSVG from "../svg/SunSVG";
 import TVSVG from "../svg/TVSVG";
 
 function Navbar() {
@@ -12,20 +15,14 @@ function Navbar() {
       <ul className="navbar-nav left">
         <HamburMenuIcon onClick={toggleNavbar} />
         <Logo />
-
-        {/* To keep <Logo/> in the middle and <HamburMenuIcon/> on the left side, 
-              so this will be on right side */}
-        <li className="nav-item">
-          <a href="#" className="nav-link">
-            🍔
-          </a>
-        </li>
+        <ToggleThemeIcon />
       </ul>
 
-      <div className="navbar-nav right">
+      <ul className="navbar-nav right">
         <NavItem navigateTo="#" icon={<TVSVG />} title="Posts" />
         <NavItem navigateTo="#" icon={<ScrollSVG />} title="Write" />
-      </div>
+        <ToggleThemeIcon />
+      </ul>
     </nav>
   );
 }
@@ -47,7 +44,45 @@ function HamburMenuIcon(props: HamburMenuIconProps) {
 function Logo() {
   return (
     <li className="nav-item">
-      <a className="nav-lik logo">NotesFlow</a>
+      <a href="/" className="nav-link logo">
+        NotesFlow
+      </a>
+    </li>
+  );
+}
+
+function ToggleThemeIcon() {
+  type AppTheme = "light" | "dark";
+  const themes: AppTheme[] = ["light", "dark"];
+
+  const [themeIcon, setThemeIcon] = useState(null);
+
+  useEffect(() => {
+    let theme: AppTheme = "dark";
+    let themeTmp = getThemeFromLocalStorage();
+    if (themeTmp) theme = themeTmp === "light" ? "light" : "dark";
+
+    // Setting theme on body
+    const bodyClass = document.body.classList;
+    bodyClass.add(`${theme}-theme`);
+  }, []);
+
+  const toggleTheme = () => {
+    const current = getThemeFromLocalStorage();
+    const next = current === "dark" ? "light" : "dark";
+    if (next === "dark") setThemeIcon(<MoonSVG />);
+    else if (next === "light") setThemeIcon(<SunSVG />);
+
+    const bodyClass = document.body.classList;
+    bodyClass.replace(`${current}-theme`, `${next}-theme`);
+    localStorage.setItem("theme", next);
+  };
+
+  return (
+    <li className="nav-item" onClick={toggleTheme}>
+      <a href="#" className="nav-link theme-icon icon">
+        <SunSVG />
+      </a>
     </li>
   );
 }
@@ -62,8 +97,8 @@ function NavItem(props: NavItemProps) {
   return (
     <li className="nav-item">
       <a href={`${props.navigateTo}`} className="nav-link">
-        {props.icon}
-        {props.title}
+        <div className="icon">{props.icon}</div>
+        <span className="text">{props.title}</span>
       </a>
     </li>
   );
