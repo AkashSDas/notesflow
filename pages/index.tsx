69045c7d9c52d06c81dd61bcef0a853a4a4289dc
AkashSDas <aakashdas368@gmail.com>
2021-04-23 12:57:07 +0530

    mv getAllFilePathsFromFolder and getContents from index.tsx to content.ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,59 +1,25 @@
-import fs from "fs";
 import { GetStaticProps } from "next";
-import path from "path";
-import Content from "../lib/content";
+import Content, { getContents } from "../lib/content";
 
 export default function Home(props) {
   return (
     <main>
-      <div>{JSON.stringify(props.filePaths)}</div>
+      <div></div>
     </main>
   );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  // Get the ".md" file paths recursively
-  const getAllFilePathsFromFolder = (basePath: string) => {
-    let paths = [];
+  let contents = getContents("contents");
+  // sorting content in descending order
+  contents = Content.sortContentsWithLastModDate(
+    getContents("contents"),
+    false
+  );
 
-    if (fs.lstatSync(basePath).isDirectory()) {
-      const folders = fs.readdirSync(basePath);
-      folders.forEach((folder: string) => {
-        let fileExtension = folder.split(".")[folder.split(".").length - 1];
-        if (fileExtension === "md") paths.push(path.join(basePath, folder));
-        else {
-          if (fs.lstatSync(path.join(basePath, folder)).isDirectory()) {
-            const innerFilePaths = getAllFilePathsFromFolder(
-              path.join(basePath, folder)
-            );
-            paths = [...paths, ...innerFilePaths];
-          }
-        }
-      });
-    } else if (fs.lstatSync(basePath).isFile()) {
-      let fileExtension = basePath.split(".")[basePath.split(".").length - 1];
-      if (fileExtension === "md") paths.push(basePath);
-    }
-
-    return paths;
-  };
-
-  // Read markdown data
-  const getContents = (): Content[] => {
-    let contents: Content[] = [];
-
-    const filePaths = getAllFilePathsFromFolder("contents");
-    filePaths.forEach((filePath: string) => {
-      const content = fs.readFileSync(filePath).toString();
-      contents.push(
-        new Content(Content.extractMetaDataFromContentStr(content, filePath))
-      );
-    });
-
-    return contents;
-  };
+  console.log(Object.keys(Content.createContentMap(contents, "contents")));
 
   return {
-    props: { contents: getContents() },
+    props: {},
   };
 };
