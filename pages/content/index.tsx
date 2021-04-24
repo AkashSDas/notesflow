5ea61508af2793cea5b0a638b509965e3fd5523e
AkashSDas <aakashdas368@gmail.com>
2021-04-24 11:22:45 +0530

    display all contents in page at route /content

--- pages/content/index.tsx
+++ pages/content/index.tsx
@@ -1,9 +1,35 @@
-function ContentsPage() {
+import { GetStaticProps } from "next";
+import MiniContentList from "../../components/content/MiniContentList";
+import Content, { getContents } from "../../lib/content";
+
+interface ContentsPageProps {
+  contents: Content[];
+}
+
+function ContentsPage(props: { contentsMap: string }) {
   return (
     <main>
-      <h1>Contents</h1>
+      <MiniContentList contentMap={JSON.parse(props.contentsMap)} />
     </main>
   );
 }
+
+export const getStaticProps: GetStaticProps = async (): Promise<{
+  props: { contentsMap: string };
+}> => {
+  // sorting content in descending order
+  let contentsMap = Content.createContentMap(
+    Content.sortContentsWithLastModDate(getContents("contents"), false),
+    "contents"
+  );
+
+  return {
+    props: {
+      // Cannot pass object directly, so stringifying it here and parsing it
+      // at the destination
+      contentsMap: JSON.stringify(contentsMap),
+    },
+  };
+};
 
 export default ContentsPage;
